beb969f449cbb088b3c93bb40aef1fbc54e437fa
aalexhidalgo <91455208+aalexhidalgo@users.noreply.github.com>
2021-10-21 20:58:45 +0200

    Initial commit

---
+++ Entregable 2/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+
+{
+    [SerializeField] private bool isGameOver;
+    private int playerHP = 150;
+    public int damage = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{playerHP} - {damage} = {playerHP - damage}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver == false)
+        {
+            if (playerHP <= 0)
+            {
+                Debug.Log("Te has quedado sin vida… GAME OVER");
+            }
+            else if (playerHP <= 30)
+            {
+                Debug.Log("¡¡¡Tienes poca vida!!!");
+            }
+            else
+            {
+                Debug.Log($"Vas bien de vida: tienes {playerHP} puntos de vida");
+            }
+                
+        }
+        else
+        {
+            Debug.Log("GAME OVER");
+        }
+
+    }
+}
